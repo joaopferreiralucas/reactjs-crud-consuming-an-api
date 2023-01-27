ea6ff9cd0896a23deca3e7fa5e3aa43444817809
Joao Ferreira Lucas <joaopferreiralucas@gmail.com>
2023-01-27 07:10:34 -0300

    axios config and example added

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 import { Title } from './styled';
 import { Container } from '../../styles/GlobalStyles';
+import axios from '../../services/axios';
 
-export default function login() {
+export default function Login() {
+  React.useEffect(() => {
+    async function getData() {
+      const res = await axios.get('/alunos');
+      const { data } = res;
+      // eslint-disable-next-line no-console
+      console.log(data);
+    }
+
+    getData();
+  }, []);
+
   return (
     <Container>
       <Title>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 
-import Login from '../pages/login';
+import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
 import MyRoute from './MyRoute';
 
