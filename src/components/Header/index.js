f859777e2f2998441732cb864522641eb35774c4
Joao P Ferreira Lucas <joaopferreiralucas@gmail.com>
2023-01-31 07:20:09 -0300

    logout feature and isLogged icon added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,24 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import {
+  FaHome, FaSignInAlt, FaUserAlt, FaCircle,
+} from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import history from '../../services/history';
+
+import * as actions from '../../store/modules/auth/actions';
 import { Nav } from './styled';
 
 export default function Header() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(actions.loginFailure());
+    history.push('/');
+  };
+
   return (
     <Nav>
       <Link to="/">
@@ -13,9 +27,17 @@ export default function Header() {
       <Link to="/register">
         <FaUserAlt size={24} />
       </Link>
-      <Link to="/login">
-        <FaSignInAlt size={24} />
-      </Link>
+      {isLoggedIn ? (
+        <Link onClick={handleLogout} to="/logout">
+          <FaSignInAlt size={24} />
+        </Link>
+      ) : (
+        <Link to="/login">
+          <FaSignInAlt size={24} />
+        </Link>
+      )}
+
+      <FaCircle size={24} color={isLoggedIn ? ('#4ef55f') : ('#f5574e')} />
     </Nav>
   );
 }
